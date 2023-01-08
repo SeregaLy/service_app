2496f25d3c597abdcf3300fc0005853169b5c5e2
SeregaLi <kaprall13rus@gmail.com>
2023-01-08 22:03:20 +0300

    day 4 - 1

--- service/services/models.py
+++ service/services/models.py
@@ -8,9 +8,10 @@ class Service(models.Model):
     name = models.CharField(max_length=50)
     full_price = models.PositiveIntegerField()
 
+
 class Plan(models.Model):
     PLAN_TYPES = (
-        ('full','Full'),
+        ('full', 'Full'),
         ('student', 'Student'),
         ('discount', 'Discount')
     )
@@ -19,12 +20,13 @@ class Plan(models.Model):
     discount_percent = models.PositiveIntegerField(default=0,
                                                    validators=[
                                                        MaxValueValidator(100)
-                                                               ])
+                                                   ])
+
 
 class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions',
                                on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions',
-                               on_delete=models.PROTECT)
+                                on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions',
-                               on_delete=models.PROTECT)
+                             on_delete=models.PROTECT)
